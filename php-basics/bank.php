fa301d6f948552ed3cf41b7ae8ae960e3e9a2ecc
Susan Buck <susan@susanbuck.net>
2014-07-01 19:46:54 -0400

    code as of break

---
+++ php-basics/bank.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>PHPiggy Bank</title>
+
+    <?php require 'logic.php' ?>
+   
+
+</head>
+
+<body>
+
+    <img alt='PHPiggy Bank Logo' src='http://making-the-internet.s3.amazonaws.com/php-phpiggy-bank.png'>
+	
+    <p>
+     You have $<?php echo $total; ?> in your piggy bank.
+    </p>
+
+</body>
+</html>
