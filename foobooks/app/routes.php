58ec998c608eb66736820d10279cd057d8d350ad
Susan Buck <susan@susanbuck.net>
2014-07-17 17:23:52 -0400

    added a quick and dirty seed route for demo purposes

--- foobooks/app/routes.php
+++ foobooks/app/routes.php
@@ -58,11 +58,14 @@ Route::post('/edit/{title}', function() {
 
 # Display add form
 Route::get('/add/', function() {
+
+	
 	
 });
 
 # Process add form
 Route::post('/add/', function() {
+	
 	
 		
 });
@@ -86,6 +89,24 @@ Route::get('/data', function() {
 	
 });
 
+
+
+
+# Quickly seed books table for demonstration purposes
+Route::get('/seed', function() {
+	
+	$query = "INSERT INTO `books` (`created_at`, `updated_at`, `title`, `author`, `published`, `cover`, `purchase_link`)
+VALUES
+	('2014-07-17 09:15:14','2014-07-17 09:15:14','The Great Gatsby','F. Scott Fiztgerald',1925,'http://img2.imagesbn.com/p/9780743273565_p0_v4_s114x166.JPG','http://www.barnesandnoble.com/w/the-great-gatsby-francis-scott-fitzgerald/1116668135?ean=9780743273565'),
+	('2014-07-17 09:15:47','2014-07-17 09:15:47','The Bell Jar','Sylvia Plath',1963,'http://img1.imagesbn.com/p/9780061148514_p0_v2_s114x166.JPG','http://www.barnesandnoble.com/w/bell-jar-sylvia-plath/1100550703?ean=9780061148514'),
+	('2014-07-17 09:16:20','2014-07-17 09:16:20','I Know Why the Caged Bird Sings','Maya Angelou',1969,'http://img1.imagesbn.com/p/9780345514400_p0_v1_s114x166.JPG','http://www.barnesandnoble.com/w/i-know-why-the-caged-bird-sings-maya-angelou/1100392955?ean=9780345514400');
+	";
+	
+	DB::statement($query);
+	
+	return $query;
+	
+});
 
 
 
